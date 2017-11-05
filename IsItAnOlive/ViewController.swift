a954e45801d0a71cf4e0c53722f4cb1efcd00327
Renan Avrahami <renan.avrahami@gmail.com>
2017-11-04 22:41:38 -0400

    Create a UIImagePickerController object and set all of its properties Add and set camera action Add UIImage outlet Add privacy permissions in plist file

--- IsItAnOlive/ViewController.swift
+++ IsItAnOlive/ViewController.swift
@@ -11,15 +11,32 @@ import CoreML
 import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
+    @IBOutlet weak var capturedImage: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
         
-        
-
     }
-
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        capturedImage.image =  userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
 }
 
